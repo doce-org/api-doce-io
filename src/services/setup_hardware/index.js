7558d1a441654418a96e9aa869af6cba8da41470
Shad <shadowrr2@gmail.com>
2017-06-24 08:41:20 +0200

    added setup hardware in memory service

---
+++ src/services/setup_hardware/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const service = require( './connection.service.extend' );
+const memory = require('feathers-memory');
+const hooks = require( './hooks' );
+
+module.exports = function () {
+
+    const app = this;
+
+    // Initialize our service with any options it requires
+    app.use( '/setup/hardwares', memory() );
+
+    const setupService = app.service( '/setup/hardwares' );
+    setupService.before( hooks.before );
+    setupService.after( hooks.after );
+
+};
